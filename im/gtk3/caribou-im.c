53fff39a79077ffcd9c83e1b738fa02fa81bef69
Nohemi Fernandez <nf68@cornell.edu>
2011-07-28 11:35:55 -0500

    Add caribou to gtk modules

--- im/gtk3/caribou-im.c
+++ im/gtk3/caribou-im.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 #include <gtk/gtkimmodule.h>
 #include "caribou-imcontext.h"
+#include <stdio.h>
 
 #define CARIBOU_LOCALDIR ""
 static const GtkIMContextInfo caribou_im_info = {
@@ -15,6 +16,13 @@ static const GtkIMContextInfo *info_list[] = {
     &caribou_im_info
 };
 
+G_MODULE_EXPORT GtkIMContext *
+gtk_module_init (gint *argc, gchar ***argv[]) {
+    g_print("Caribou module loaded \n");
+    CaribouIMContext *context = caribou_im_context_new ();
+        return (GtkIMContext *) context;
+}
+
 G_MODULE_EXPORT const gchar*
 g_module_check_init (GModule *module)
 {
@@ -23,31 +31,3 @@ g_module_check_init (GModule *module)
                                0);
 }
 
-G_MODULE_EXPORT void
-im_module_init (GTypeModule *type_module)
-{
-    g_type_module_use (type_module);
-}
-
-G_MODULE_EXPORT void
-im_module_exit (void)
-{
-}
-
-G_MODULE_EXPORT GtkIMContext *
-im_module_create (const gchar *context_id)
-{
-    if (g_strcmp0 (context_id, "caribou") == 0) {
-        CaribouIMContext *context = caribou_im_context_new ();
-        return (GtkIMContext *) context;
-    }
-    return NULL;
-}
-
-G_MODULE_EXPORT void
-im_module_list (const GtkIMContextInfo ***contexts,
-                gint                     *n_contexts)
-{
-    *contexts = info_list;
-    *n_contexts = G_N_ELEMENTS (info_list);
-}
